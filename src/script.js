659216e57378c0f88533118f0ea08a3d4ed5794b
natacha <nat_bartu@hotmail.com>
2022-08-02 12:49:59 +1000

    fix the relative path for the sounds

--- src/script.js
+++ src/script.js
@@ -1,10 +1,14 @@
-const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong", "bells"];
 
 sounds.forEach((sound) => {
   const btn = document.createElement("button");
   btn.classList.add("btn");
 
   btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    document.getElementById(sound).play();
+  });
 
   document.getElementById("buttons").appendChild(btn);
 });
